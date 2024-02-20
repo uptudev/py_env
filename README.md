845a9df701ed98142c4cdf0a7db1edbaf3a21831
uptu <uptu@uptu.dev>
2024-02-19 17:46:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# py_shell
+
+**[Short, memorable description of your project]**
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [License](#license)
+* [Additional Information](#additional-information)
+
+## Installation
+
+**Clearly describe how to install your project.** This may involve specifying dependencies, prerequisites, and build instructions. Use code blocks, links, and step-by-step guides for clarity.
+
+## Usage
+
+**Provide clear and concise instructions on how to use your project.** Explain its functionalities, features, and common use cases. Include examples, screenshots, or GIFs if helpful.
+
+**Tips:**
+
+* Break down instructions into logical steps.
+* Use bullet points for succinct explanations.
+* Consider creating a separate "Getting Started" guide for beginners.
+
+## Contributing
+
+**Outline your contribution guidelines.** Explain how users can contribute to your project, whether through code, bug reports, or documentation improvements. Specify preferred code style, pull request format, and testing procedures.
+
+## License
+
+**Specify the license under which your project is distributed.** Use clear and concise language, and link to the full license text in the `LICENSE` file.
+
+## Additional Information
+
+**Include any other relevant information you want to share.** This could be links to related projects, documentation, support channels, or your contact information.
+
+**Remember:**
+
+* Keep your README.md file concise and focused.
+* Use clear headings, formatting, and visuals for readability.
+* Update your README.md file regularly to reflect changes in your project.
---
+++ src/lib.rs
@@ -0,0 +1,88 @@
+use std::io::{stdout, stderr, Stdout, Stderr};
+use std::path::PathBuf;
+
+// pandora's box of self-mutating code
+pub struct PyEnv {
+    path: PathBuf,
+    std_out: Box<dyn Fn() -> Stdout>,
+    std_err: Box<dyn Fn() -> Stderr>,
+} 
+
+impl PyEnv {
+    // Constructor for piping stdout and stderr to a custom stream.
+    // Use `at()` if you want to inherit the streams.
+    pub fn new(
+        path: &str, 
+        std_out: Box<dyn Fn() -> Stdout>,
+        std_err: Box<dyn Fn() -> Stderr>,
+    ) -> Self {
+        let path = std::path::PathBuf::from(path);
+        Self {path, std_out, std_err}
+    }
+
+    /// Constructor inheriting default stdout and stderr; use `new()` to customize the streams.
+    pub fn at(path: &str) -> Self {
+        Self::new(path, Box::new(stdout), Box::new(stderr))
+    }
+
+    /// Installs a package in the PyEnv, returning itself to easily chain dependencies.
+    pub fn install_package(&self, package_name: &str) -> &Self {
+        let mut handle = std::process::Command::new("python")
+            .args([
+                "-m", 
+                "pip", 
+                "install",
+                package_name,
+                "--target",
+                self.path
+                    .join("site-packages")
+                    .as_os_str()
+                    .to_str()
+                    .unwrap()])
+            .stdout((self.std_out)())
+            .stderr((self.std_err)())
+            .spawn()
+            .expect("Error installing package");
+        handle.wait().unwrap();
+        &self
+    }
+    
+    /// Executes arbitrary code in the PyEnv, returning itself to easily chain runs.
+    pub fn execute(&self, code: &str) -> &Self {
+        std::env::set_var("PYTHONPATH", self.path.join("site-packages"));
+        let mut handle = std::process::Command::new("python")
+            .args([
+            "-c",
+            code])
+            .stdout((self.std_out)())
+            .stderr((self.std_err)())
+            .spawn()
+            .expect("Error running code");
+        handle.wait().unwrap();
+        &self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_install() {
+        PyEnv::at("./pytest")
+        .install_package("faker");
+    }
+
+    #[test]
+    fn test_run() {
+        PyEnv::at("./pytest")
+        .execute("print('hello world')");
+    }
+
+    #[test]
+    fn test_install_run() {
+        PyEnv::at("./pytest")
+        .install_package("faker")
+        .execute("import faker; print(faker.Faker().name())");
+    }
+}
